570b9bd688c9df620effcac1b25dece237036984
niepra01 <rolf.niepraschk@ptb.de>
2014-04-09 16:33:04 +0200

    ...

--- v-test-x.js
+++ v-test-x.js
@@ -16,7 +16,7 @@ var HOST = '172.30.56.65';
 var DEVICE = 'gpib0,10';
 
 var CMD = '*IDN?\n';
-var CMD = 'DATA?';
+///var CMD = 'DATA?\n';
 var READ_TIMEOUT = 2047;
 var VXI11_DEFAULT_TIMEOUT = 10000; /* in ms */
 var VXI11_READ_TIMEOUT = 2000; /* in ms */
@@ -27,6 +27,9 @@ var RPC_VERSION = 2;
 var DEVICE_ASYNC_PROG = 0x0607b0;
 var DEVICE_ASYNC_VERS = 1;
 var DEVICE_ABORT = 1;
+
+// Device intr
+var DEVICE_INTR = 0x0607b1;
 
 // Device core
 var DEVICE_CORE_PROG = 0x0607af;
@@ -46,6 +49,7 @@ var DEVICE_DOCMD = 22;
 var DESTROY_LINK = 23;
 var CREATE_INTR_CHAN = 25;
 var DESTROY_INTR_CHAN = 26;
+var DEVICE_INTR_SRQ = 30;
 
 // Program number, version and port number
 var PMAP_PROG = 100000;
@@ -82,7 +86,7 @@ function createClient(clnt, clbk) {
   buf.writeUInt32BE(0x00000000, 36);  //Verfifier
   buf.writeUInt32BE(DEVICE_CORE_PROG, 40);  //Portmap Getportcall/Program Unknow
   buf.writeUInt32BE(DEVICE_CORE_VERS, 44);  //Portmap Getportcall Version
-  buf.writeUInt32BE(IPPROTO_TCP, 48);  //Portmap Getport TCP
+  buf.writeUInt32BE(IPPROTO_TCP, 48); //Portmap Getport TCP
   buf.writeUInt32BE(0x00000000, 52);  //Portmap Getpor Port
   // TODO: Alles nachprüfen; auch Benennung.
   console.log('LinkParms: ' + util.inspect(buf));
