b7aab3187c653576cd1fc7848f4e1b44498917c4
niepra01 <rolf.niepraschk@ptb.de>
2014-04-25 10:12:28 +0200

    ...

--- vxi11-test.js
+++ vxi11-test.js
@@ -4,17 +4,16 @@
   Rolf Niepraschk, Rolf.Niepraschk@gmx.de, 2014-04-25
 */
 
-//var vxi = require('./lib/vxi11.js');
 var vxiTransceiver = require('./lib/vxi11.js').vxiTransceiver
 var util = require('util');
 
-var HOST = 'e75465';
+var HOST = '172.30.56.65';
 var DEVICE = 'gpib0,10';
 
-///var CMD = 'DATA?\n';
-///var CMD = '*ESR?';
-var CMD = '*RST;*OPC?';
-var CMD = '*IDN?';
+var CMD = 'DATA?';
+var CMD = '*ESR?';
+var CMD = '*RST;*OPC?\n';
+//var CMD = '*IDN?\n';
 
 // example call with default parameters (no logging)
 vxiTransceiver(HOST, DEVICE, CMD, function(result) {
@@ -25,9 +24,9 @@ var options = {
   host: HOST,
   device: DEVICE,
   command: CMD,
-  readTimeout: 1000,
-  ioTimeout: 6000,
-  lockTimeout: 6000,
+  readTimeout: 1000, // default:  2000ms
+  ioTimeout: 6000,   // default: 10000ms
+  lockTimeout: 6000, // default: 10000ms
   lockDevice: true,
   //terminationChar: '\n',
   logger: { log: console.log, error: console.error }
@@ -37,4 +36,7 @@ var options = {
 vxiTransceiver(options, function(result) {
   console.log('*** result 2: »' + result + '«');
 });
+
+
+
 
